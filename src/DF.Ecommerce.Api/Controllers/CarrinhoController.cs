1b511661ff0599a36e3749906a48d09efaf1f37a
Gustavo Diogenes de Souza <gustavodiogenes@frwk.com.br>
2023-02-14 13:03:19 -0300

    Criação dos Controllers

---
+++ src/DF.Ecommerce.Api/Controllers/CarrinhoController.cs
@@ -0,0 +1,25 @@
+﻿using DF.Ecommerce.Domain.Interfaces.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DF.Ecommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrinhoController : ControllerBase
+    {
+        private readonly ICarrinhoRepository _carrinhoRepository;
+
+        public CarrinhoController(ICarrinhoRepository carrinhoRepository)
+        {
+            _carrinhoRepository = carrinhoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterCarrinho (string cpf)
+        {
+            var carrinho = await _carrinhoRepository.ObterCarrinho(cpf);
+            return Ok(carrinho);
+        }
+    }
+}
